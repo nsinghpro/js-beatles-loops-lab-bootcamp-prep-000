6f4dd17d17826d7271f784ae22fea5b2a1a8e7c9
nirmal singh <nsinghhomes@gmail.com>
2018-12-12 06:05:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  let array =[ ];
+  for (let i=0; i < musicians.length; i++){
+    const string = musicians[i]+" plays "+ instruments[i];
+    array.push(string);
+  }
+  return array;
+}
